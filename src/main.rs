bfc12de85bfe6a3c801f1d0ee5d296f5e7aabb01
devnote-dev <devnote.dev75@gmail.com>
2023-06-28 16:03:22 +0100

    feat: replace main with repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clip::{lexer::Lexer, parser::Parser};
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    loop {
+        print!(">> ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let tokens = Lexer::new(&input).lex();
+        match Parser::new(tokens).parse() {
+            Ok(p) => {
+                for stmt in p.statements {
+                    println!("{:?}", stmt);
+                }
+            }
+            Err(e) => eprintln!("{}", e),
+        }
+    }
 }
